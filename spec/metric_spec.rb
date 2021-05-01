45d46ec81bfb62ea51b11291cbd22d67ec96f27f
Mervelin <rodin.sergiusz@gmail.com>
2021-05-01 10:56:44 +0200

    Add spec that will cover ordering of metric list

--- spec/metric_spec.rb
+++ spec/metric_spec.rb
@@ -1,7 +1,6 @@
 require './lib/metric'
 
 describe Metric do
-
   subject(:metric) { Metric.new }
 
   describe '#increment' do
@@ -11,6 +10,21 @@ describe Metric do
       metric.increment(element)
 
       expect(metric.list).to eql ({ 'abc' => 1 })
+    end
+  end
+
+  describe '#order_descending' do
+    let(:first_element) { 'abc' }
+    let(:second_element) { 'def' }
+
+    before do
+      metric.increment(first_element)
+      metric.increment(second_element)
+      metric.increment(second_element)
+    end
+
+    it 'returns elements ordered descending based on value' do
+      expect(metric.order_descending).to eql ({ 'def' => 2, 'abc' => 1 })
     end
   end
 end
