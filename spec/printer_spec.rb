5a03fb3d6dd6dbc086f23cb6744f6c0befbc1389
Mervelin <rodin.sergiusz@gmail.com>
2021-05-01 11:12:05 +0200

    Add printer util class and fix spec

--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -1,11 +1,13 @@
-describe Printer
+require './lib/printer'
+
+describe Printer do
   let(:list) { [['apple', 2], ['orange', 1]] }
   let(:text) { 'sold' }
 
   subject(:print) { Printer.print(list, text) }
   describe '.print' do
     it 'prints entries with text appended' do
-      expect(print).to output('apple 2 sold')
+      expect { print }.to output("\"apple 2 sold\"\n\"orange 1 sold\"\n").to_stdout
     end
   end
 end
