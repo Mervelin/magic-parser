5d0f568178c65f4fb3fe2625c928e94f7b804610
Mervelin <rodin.sergiusz@gmail.com>
2021-05-01 10:49:10 +0200

    Use helpers classes in main Metrics class

--- lib/metrics.rb
+++ lib/metrics.rb
@@ -1,22 +1,51 @@
+require_relative './entry'
+require_relative './metric'
+
 class Metrics
   def initialize(entries)
     @entries = entries
-    @popular = {}
+    @popular = Metric.new
+    @unique = Metric.new
+    @paths = {}
   end
 
-  attr_reader :entries, :popular
-
   def popular_list
-    entries.each do |entry|
-      url = entry.split(' ').first
+    entries.each do |line|
+      entry = Entry.new(line)
 
-      if popular[url]
-        popular[url] += 1
-      else
-        popular[url] = 1
-      end
+      popular.increment(entry.url)
     end
 
-    popular
+    popular.list
+  end
+
+  def unique_list
+    entries.each do |line|
+      entry = Entry.new(line)
+
+      calculate_unique(entry)
+    end
+
+    unique.list
+  end
+
+  private
+
+  attr_reader :entries, :popular, :unique, :paths
+
+  def calculate_unique(entry)
+    return if path_visited?(entry)
+
+    unique.increment(entry.url)
+
+    add_path_to_visited(entry)
+  end
+
+  def path_visited?(entry)
+    paths[entry.url]&.include?(entry.ip)
+  end
+
+  def add_path_to_visited(entry)
+    (paths[entry.url] ||= []) << entry.ip
   end
 end
