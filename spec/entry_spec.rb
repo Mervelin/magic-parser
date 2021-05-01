72ffc082212fc18a682caee234914f7db0291df4
Mervelin <rodin.sergiusz@gmail.com>
2021-05-01 10:33:26 +0200

    Add tests for helper classes to divide reponsibilities

---
+++ spec/entry_spec.rb
@@ -0,0 +1,19 @@
+describe Entry do
+  context 'when provided with proper line from log file' do
+    path(:line) { '/home 1.1.1.1' }
+
+    subject(:entry) { Entry.new(line) }
+
+    describe '#url' do
+      it 'returns first part of line' do
+        expect(entry.url).to eql '/home'
+      end
+    end
+
+    describe '#ip' do
+      it 'returns second part of line' do
+        expect(entry.ip).to eql '1.1.1.1'
+      end
+    end
+  end
+end
---
+++ spec/metric_spec.rb
@@ -0,0 +1,14 @@
+describe Metric do
+
+  subject(:metric) { Metric.new }
+
+  describe '#increment' do
+    let(:element) { 'abc' }
+
+    it 'increases the count of element' do
+      metric.increment(element)
+
+      expect(metric.list).to eql { 'abc' => 1 }
+    end
+  end
+end
